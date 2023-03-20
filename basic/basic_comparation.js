e84a05a9495de97f3ccb71d47f6ad3e79db7ab24
Iryna Vaskiv <Iryna.Vaskiv@enghouse.com>
2023-03-20 16:31:46 +0200

    comparation

---
+++ basic/basic_comparation.js
@@ -0,0 +1,21 @@
+//
+let x = 1, y = 3
+console.log(' y - x:', y - x ) // 2
+
+//
+let n = 2 + '1'
+console.log('typeof n: ', typeof n) // str
+
+//
+let n1 = 2 + +'1'
+console.log('typeof n1: ',typeof n1) // number
+
+//
+let n2 = 6 - '2'
+console.log('typeof n2: ',typeof n2) // number, because str can not minuses
+
+// counter 1
+let counter = 1;
+let a = ++counter; // (*)
+
+console.log('a: ', a)
